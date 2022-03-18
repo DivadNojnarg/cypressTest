30482021774a784f073ec8a96df4d176cf6efea1
DivadNojnarg <dgranjon@ymail.com>
2022-03-18 23:42:08 +0100

    added working example

--- app/main.R
+++ app/main.R
@@ -1,20 +1,49 @@
 box::use(
-  shiny[bootstrapPage, moduleServer, NS, renderText, tags, textOutput],
+  shiny[
+    fluidPage,
+    moduleServer,
+    NS,
+    tableOutput,
+    selectInput,
+    renderTable,
+    observeEvent,
+    showNotification,
+    req
+  ]
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    tags$h3(
-      textOutput(ns("message"))
-    )
+  fluidPage(
+    selectInput(
+      selectize = FALSE,
+      ns("variable"), "Variable:",
+      c(
+        "Cylinders" = "cyl",
+        "Transmission" = "am",
+        "Gears" = "gear"
+      )
+    ),
+    tableOutput(ns("data"))
   )
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderText("Hello!")
+    output$data <- renderTable(
+      {
+        datasets::mtcars[, c("mpg", input$variable), drop = FALSE]
+      },
+      rownames = TRUE
+    )
+
+    observeEvent(
+      req(input$variable == "cyl"),
+      {
+        showNotification("Hello")
+      }
+    )
   })
 }
--- tests/cypress/integration/app.spec.js
+++ tests/cypress/integration/app.spec.js
@@ -1,7 +1,45 @@
 describe('app', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
-  it('starts', () => {})
-})
+  it('starts', () => {});
+
+  it('has correct table columns', () => {
+    /* Basic table structure */
+    cy
+      .get('#app-data > table > thead > tr > th:nth-child(1)')
+      .should('have.text', '  ');
+
+    cy
+      .get('#app-data > table > thead > tr > th:nth-child(2)')
+      .should('have.text', ' mpg ');
+  });
+
+  it('shows notification', () => {
+    /* Notification is visible */
+    cy
+      .get('#shiny-notification-panel')
+      .should('be.visible');
+  });
+
+  it('has working select input', function() {
+    /* Change select to Transmission */
+    cy
+      .get('select')
+      .select('am');
+
+    cy
+      .get('#app-data > table > thead > tr > th:nth-child(3)')
+      .should('have.text', ' am ');
+
+    /* Change select to Gears */
+    cy
+      .get('select')
+      .select('gear');
+
+    cy
+      .get('#app-data > table > thead > tr > th:nth-child(3)')
+      .should('have.text', ' gear ');
+  });
+});
